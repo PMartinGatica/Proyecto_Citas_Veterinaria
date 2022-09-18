56d1a55d2339e410b71de83566af3827af31a047
Pablo <pablomartin.gatica@gmail.com>
2022-09-18 11:05:31 -0300

    IntrgracionVerMascotaComponent

--- src/app/components/ver-mascota/ver-mascota.component.ts
+++ src/app/components/ver-mascota/ver-mascota.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MascotaService } from '../../services/mascota.service';
+import { Mascota } from '../../interfaces/mascota';
+import { MatCardLgImage } from '@angular/material/card';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ver-mascota',
   templateUrl: './ver-mascota.component.html',
   styleUrls: ['./ver-mascota.component.css']
 })
-export class VerMascotaComponent implements OnInit {
+export class VerMascotaComponent implements OnInit, OnDestroy {
+  id!: number;
+  mascota !: Mascota;
+  /*PIPE ASYNC  
+  mascota$ = observable<Mascota>;
+  */
+ loading: boolean = false;
+ routeSub !: Subscription; 
 
-  constructor() { }
+  constructor(private _mascotaService : MascotaService,
+              private aRoute: ActivatedRoute
+    ) {/* this.id = +this.aRoute.snapshot.paramMap.get('id')!; */ /* Opcion para obtener el ID por ruta */
+   }
 
   ngOnInit(): void {
+   this.routeSub =  this.aRoute.params.subscribe(data=> {
+      this.id = data['id'];
+      this.obtenerMascota();
+    });
+    
+    /* PIPE ASYNC
+    this.obtenerMascota();
+    */
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
+
+  obtenerMascota(){
+    this.loading = true;
+    this._mascotaService.getMascota(this.id).subscribe(data => {
+      //console.log(data);
+    this.mascota = data;
+    this.loading = false; 
+    });
   }
 
 }
